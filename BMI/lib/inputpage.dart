f20ada775aacc72c2c1494e8a9108237ae842d30
Suleyman Anwar <suleman.anwar.424@gmail.com>
2022-04-10 12:11:15 +0500

    UI design

--- BMI/lib/inputpage.dart
+++ BMI/lib/inputpage.dart
@@ -11,6 +11,66 @@ class _inputpageState extends State<inputpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child:Row(
+            children: <Widget>[
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(15.0)
+                ),
+              ) ),
+
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(15.0)
+                ),
+              ))
+
+            ],
+          )
+          ),
+
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(15.0)
+            ),
+          )
+          ),
+
+          Expanded(child:Row(
+            children: <Widget>[
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(15.0)
+                ),
+              )),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(15.0)
+                ),
+              ))
+
+            ],
+          )
+          ),
+
+        ],
+      ),
 
     );
   }
