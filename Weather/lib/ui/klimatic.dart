5f26b225cf4748f7b2e45d039afda48425e4af6d
Suleyman Anwar <suleman.anwar.424@gmail.com>
2022-04-17 17:22:04 +0500

    Future and Async and api url

--- Weather/lib/ui/klimatic.dart
+++ Weather/lib/ui/klimatic.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../utils/api.dart' as utils;
+
+import '../constants.dart';
 
 class klimatic extends StatefulWidget {
 
@@ -20,6 +24,50 @@ class _klimaticState extends State<klimatic> {
               icon: Icon(Icons.menu))
         ],
       ),
+
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Image(
+              image: AssetImage('images/umbrella.png'),
+              height: 1250.0,
+              width: 600.0,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            alignment: Alignment.topRight,
+            margin: EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
+            child: Text(
+              '${_cityEntered == null ? util.defaultCity : _cityEntered}',
+              style: cityStyle(),
+            ),
+          ),
+          Center(
+            child: Image(
+              image: AssetImage('images/light_rain.png'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(30.0, 90.0, 0.0, 0.0),
+            child: updateTempWidget(
+                '${_cityEntered == null ? util.defaultCity : _cityEntered}'),
+          ),
+        ],
+      ),
     );
   }
+
+
+  Future<Map> getWeather(String appId, String city) async {
+    String apiUrl =
+        'http://api.openweathermap.org/data/2.5/weather?q=$city&appid='
+        '${utils.apiId}&units=imperial';
+    //http://api.openweathermap.org/data/2.5/weather?q=vehari&appid=ee595cbdb0db0ef5d11b9caf5a8eb1ea&units=metric
+
+    return json.decode(response.body);
+  }
+
 }
+
+
