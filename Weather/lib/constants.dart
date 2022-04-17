c599108ea379793cffc8c5ca210eb4bb17a0d34f
Suleyman Anwar <suleman.anwar.424@gmail.com>
2022-04-17 17:27:50 +0500

    Future and Async and url api

---
+++ Weather/lib/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+TextStyle city(){
+  return TextStyle(
+    color: Colors.white,
+    fontSize: 23,
+    fontStyle: FontStyle.italic,
+  );
+}
+
+
+TextStyle tempstyle() {
+  return TextStyle(
+      color: Colors.white,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      fontSize: 49.9);
+}
+
+TextStyle extradata() {
+  return TextStyle(
+      color: Colors.white70, fontStyle: FontStyle.normal, fontSize: 17.0);
+}
---
+++ Weather/lib/utils/api.dart
@@ -0,0 +1,2 @@
+final apiId = "237899af79d1b336cb3b6d08b549ec3f";
+final defaultCity = "Vehari";
