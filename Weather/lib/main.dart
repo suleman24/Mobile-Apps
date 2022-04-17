b845d99c10262985113284f767d1e7d2ca884ddb
Suleyman Anwar <suleman.anwar.424@gmail.com>
2022-04-17 10:07:31 +0500

    added images

--- Weather/lib/main.dart
+++ Weather/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:weather/ui/klimatic.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title:'Klimatic',
+      debugShowCheckedModeBanner: false,
       home: new klimatic(),
     )
 
---
+++ Weather/lib/ui/klimatic.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class klimatic extends StatefulWidget {
+
+  @override
+  _klimaticState createState() => _klimaticState();
+}
+
+class _klimaticState extends State<klimatic> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Klimatic'),
+        backgroundColor: Colors.white,
+        actions: [],
+      ),
+    );
+  }
+}
