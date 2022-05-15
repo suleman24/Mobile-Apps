5bd33a3bffa83377582de737c25ae908c6980847
Suleyman Anwar <63181973+suleman24@users.noreply.github.com>
2022-05-15 06:24:48 -0700

    Added Lottie splash screen

--- Willingness Form (Semester Project)/willingness_form/lib/main.dart
+++ Willingness Form (Semester Project)/willingness_form/lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:willingness_form/splashscreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( splashscreen());
 }
+
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello World'),
+    );
+  }
+}
+
 
 
 
---
+++ Willingness Form (Semester Project)/willingness_form/lib/splashscreen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'main.dart';
+class splashscreen extends StatefulWidget {
+  const splashscreen({Key? key}) : super(key: key);
+
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(1.18),
+
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(78.0),
+                      child: AnimatedTextKit(
+                        animatedTexts: [
+                          WavyAnimatedText(
+                            'Willingness Form CUI',
+                            textStyle: const TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                          WavyAnimatedText('by Suleman & Umer'),
+
+                        ],
+
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Lottie.asset(
+                      'assets/lottie.json',
+                      height: 400,
+                      width: 1200,
+                      animate: true,
+
+                      controller: _controller,
+                      onLoaded: (composition) {
+                        _controller
+                          ..duration = composition.duration
+                          ..forward().whenComplete(() => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => MyApp()),
+                          ));
+                        // Navigator.pop(context);
+
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+
+    );
+
+  }
+}
