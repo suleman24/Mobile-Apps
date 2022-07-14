21bb5325236b990eda8d89234b025166d5bdd501
Suleyman Anwar <63181973+suleman24@users.noreply.github.com>
2022-07-14 19:00:01 +0500

    Teacher Subject & Class Pdf Done

--- lab_final/manager/lib/download/download.dart
+++ lab_final/manager/lib/download/download.dart
@@ -5,9 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_share/flutter_share.dart';
 import 'package:manager/download/student_contact.dart';
+import 'package:manager/download/student_fee.dart';
+import 'package:manager/download/student_personal.dart';
+import 'package:manager/download/student_personal_Pdfservices.dart';
+import 'package:manager/download/student_sub_class.dart';
+import 'package:manager/download/teacher_personal_Pdfservices.dart';
+import 'package:manager/download/teacher_sub_class_Pdfservices.dart';
 import '../authentication.dart';
 import 'teacher_contact_Pdfservices.dart';
-import 'download_teacher_contacts.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
@@ -21,30 +26,44 @@ class download extends StatefulWidget {
 
 class _downloadState extends State<download> {
 
-  final decor = TextStyle(
-    color: Colors.white,
-    fontWeight: FontWeight.bold,
-  );
-  final boxDecor = BoxDecoration(
-    color: Colors.blue.shade400,
-    borderRadius: BorderRadius.circular(
-      50,
-    ),
-  );
-  bool uploading = false;
-  bool downloading = false;
-
-
-  List Pdfdata = [];
-
-  getData() async {
+  List teacher_contatct_data = [];
+  getteachercontact() async {
     await FirebaseFirestore.instance.collection('manager').doc(AuthenticationHelper().getID()).collection('teachers').get().then((value) {
       for(var i in value.docs) {
-        Pdfdata.add(i.data());
+        teacher_contatct_data.add(i.data());
       }
     });
   }
-  final teacher_contact_Pdfservices _pdfservices=teacher_contact_Pdfservices();
+  final teacher_contact_Pdfservices _pdfservices_teachercontact=teacher_contact_Pdfservices();
+
+
+
+
+  List teacher_personal_data = [];
+  getteacherpersonal() async {
+    await FirebaseFirestore.instance.collection('manager').doc(AuthenticationHelper().getID()).collection('teachers').get().then((value) {
+      for(var i in value.docs) {
+        teacher_personal_data.add(i.data());
+      }
+    });
+  }
+  final teacher_personal_Pdfservices _pdfservices_teacherpersonal=teacher_personal_Pdfservices();
+
+
+
+
+
+  List teacher_sub_class_data = [];
+  getteachersub_class() async {
+    await FirebaseFirestore.instance.collection('manager').doc(AuthenticationHelper().getID()).collection('teachers').get().then((value) {
+      for(var i in value.docs) {
+        teacher_sub_class_data.add(i.data());
+      }
+    });
+  }
+  final teacher_sub_class_Pdfservices _pdfservices_teacher_sub_class=teacher_sub_class_Pdfservices ();
+
+
 
 
   @override
@@ -67,11 +86,68 @@ class _downloadState extends State<download> {
                 padding: const EdgeInsets.all(10.0),
                 child: InkWell(
                   onTap: () async {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => student_fee()));
 
-                    await getData();
-                    final data=await _pdfservices.createPdf(Pdfdata);
-                    _pdfservices.saveAndLanchFile(data, "teachercontact.pdf");
-                    Pdfdata.clear();
+                  },
+                  child: Material(
+                    color: Colors.teal[300],
+                    elevation: 10.0,
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              'Student Fee',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.white,
+
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10.0,
+                            ),
+                            child: Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(100.0),
+                              child: Container(
+                                height: 50.0,
+                                width: 50.0,
+                                child: ClipOval(
+                                  child: Image(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      'images/download.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: InkWell(
+                  onTap: () async {
+
+                    await getteachercontact();
+                    final data=await _pdfservices_teachercontact.createPdf(teacher_contatct_data);
+                    _pdfservices_teachercontact.saveAndLanchFile(data, "Teacher_class_&_subjeect.pdf");
+                    teacher_contatct_data.clear();
+
                   },
                   child: Material(
                     color: Colors.teal[300],
@@ -121,13 +197,6 @@ class _downloadState extends State<download> {
                   ),
                 ),
               ),
-
-
-
-
-
-
-
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: InkWell(
@@ -154,6 +223,265 @@ class _downloadState extends State<download> {
                                 fontWeight: FontWeight.w700,
                               ),
                             ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10.0,
+                            ),
+                            child: Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(100.0),
+                              child: Container(
+                                height: 50.0,
+                                width: 50.0,
+                                child: ClipOval(
+                                  child: Image(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      'images/download.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: InkWell(
+                  onTap: () async {
+
+                    await getteacherpersonal();
+                    final data=await _pdfservices_teacherpersonal.createPdf(teacher_personal_data);
+                    _pdfservices_teacherpersonal.saveAndLanchFile(data, "Teacher_Personal_Info.pdf");
+                    teacher_personal_data.clear();
+
+                  },
+                  child: Material(
+                    color: Colors.teal[300],
+                    elevation: 10.0,
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              'Teachers Details',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.white,
+
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10.0,
+                            ),
+                            child: Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(100.0),
+                              child: Container(
+                                height: 50.0,
+                                width: 50.0,
+                                child: ClipOval(
+                                  child: Image(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      'images/download.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: InkWell(
+                  onTap: () async {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => student_personal()));
+
+                  },
+                  child: Material(
+                    color: Colors.teal[300],
+                    elevation: 10.0,
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              'Student Details',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.white,
+
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10.0,
+                            ),
+                            child: Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(100.0),
+                              child: Container(
+                                height: 50.0,
+                                width: 50.0,
+                                child: ClipOval(
+                                  child: Image(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      'images/download.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: InkWell(
+                  onTap: () async {
+
+
+                    await getteachersub_class();
+                    final data=await _pdfservices_teacher_sub_class.createPdf(teacher_sub_class_data);
+                    _pdfservices_teacher_sub_class.saveAndLanchFile(data, "teachers_subject_&_class.pdf");
+                    teacher_contatct_data.clear();
+
+                  },
+                  child: Material(
+                    color: Colors.teal[300],
+                    elevation: 10.0,
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Center(
+                                child: Text(
+                                  'Teachers Class',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.white,
+
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              Center(
+                                child: Text(
+                                  '& Subjects',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.white,
+
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10.0,
+                            ),
+                            child: Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(100.0),
+                              child: Container(
+                                height: 50.0,
+                                width: 50.0,
+                                child: ClipOval(
+                                  child: Image(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      'images/download.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => student_sub_class()));
+                  },
+                  child: Material(
+                    color: Colors.teal[300],
+                    elevation: 10.0,
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Center(
+                                child: Text(
+                                  'Students Subjects',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.white,
+
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              Center(
+                                child: Text(
+                                  'Classwise',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                           Padding(
                             padding: EdgeInsets.symmetric(
