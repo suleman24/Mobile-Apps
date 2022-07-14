787873f395e3e5e9476c30e173bbbc6d94a938ff
Suleyman Anwar <63181973+suleman24@users.noreply.github.com>
2022-07-14 08:29:42 +0500

    Student image in all Fee Screens Done

---
+++ lab_final/manager/lib/Fees/fee.dart
@@ -0,0 +1,116 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../authentication.dart';
+import 'classfee.dart';
+
+
+
+
+class fee extends StatefulWidget {
+  @override
+  _feeState createState() => _feeState();
+}
+
+class _feeState extends State<fee> {
+
+  var name;
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Click on any class to continue'),
+        backgroundColor: Colors.blue[800],
+      ),
+      body:
+      StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('manager').doc(AuthenticationHelper().getID()).collection('classes').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+              itemCount: snapshot.data.docs.length,
+              itemBuilder: (context, int index) {
+                return Padding(
+                  padding:
+                  EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                  child: CardList(snapshot: snapshot.data,index: index),
+                );
+              });
+        },
+      ),
+
+    );
+
+  }
+
+}
+
+
+class CardList extends StatelessWidget {
+  CardList({required this.snapshot,required this.index});
+  final QuerySnapshot snapshot;
+  final int index;
+
+
+  @override
+  Widget build(BuildContext context) {
+    var docid=snapshot.docs[index].id;
+    var name=snapshot.docs[index]['name'];
+    var fee=snapshot.docs[index]['fee'];
+
+    TextEditingController updatee = TextEditingController();
+    return GestureDetector(
+      onTap: (){
+
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => classfee(name: name,fee:fee,docid:docid)));
+      },
+      child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.blue,  // red as border color
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(2))),
+
+          child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    // changing from 200 to 150 as to look better
+                    height: 50.0,
+                    width: 50.0,
+                    child: ClipOval(
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: AssetImage(
+                          'images/class.jpg',
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Text(
+                    snapshot.docs[index]['name'],
+                    style:
+                    TextStyle(fontSize: 25, color:Colors.blue[900], fontWeight: FontWeight.w900),
+                  ),
+                ],
+              ))),
+    );
+  }
+}
