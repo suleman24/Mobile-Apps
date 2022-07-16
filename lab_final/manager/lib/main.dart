585d367a71b16668733db05854f8adf2cb1daedb
Suleyman Anwar <63181973+suleman24@users.noreply.github.com>
2022-07-16 10:16:40 +0500

    Apps Icons & Splash Screens Done

--- lab_final/manager/lib/main.dart
+++ lab_final/manager/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 import 'check.dart';
 
@@ -9,5 +10,27 @@ Future<void> main() async {
   await Firebase.initializeApp();
 
   runApp(
-      MaterialApp(debugShowCheckedModeBanner: false, home: Check()));
+      MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+}
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:Scaffold(
+          body:
+          SplashScreenView(
+            navigateRoute: Check(),
+            duration: 4000,
+            imageSize: 200,
+            imageSrc: "images/splash.png",
+            backgroundColor: Colors.blue[500],
+            text: 'Welcome to Academia',textStyle:TextStyle(fontSize: 30,color: Colors.amber[300]),
+          ),
+        )
+    );
+  }
 }
--- lab_final/teacher_student/lib/main.dart
+++ lab_final/teacher_student/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 import 'academies.dart';
 
 Future<void> main() async {
@@ -7,5 +8,29 @@ Future<void> main() async {
   await Firebase.initializeApp();
 
   runApp(
-      MaterialApp(debugShowCheckedModeBanner: false, home: academies()));
+      MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+}
+
+
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:Scaffold(
+          body:
+          SplashScreenView(
+            navigateRoute: academies(),
+            duration: 3000,
+            imageSize: 200,
+            imageSrc: "images/splash.png",
+            backgroundColor: Colors.blue[500],
+            text: 'Welcome to Academia',textStyle:TextStyle(fontSize: 30,color: Colors.amber[300]),
+          ),
+        )
+    );
+  }
 }
